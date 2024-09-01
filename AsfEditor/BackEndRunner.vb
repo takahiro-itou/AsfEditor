a886af6e9449fe292c76cb6b19c01b733a6f0847
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-01 17:58:06 +0900

    関数の宣言を追加

---
+++ AsfEditor/BackEndRunner.vb
@@ -0,0 +1,39 @@
+Public Module BackEndRunner
+
+
+Public Function launchProcess(
+        ByVal sCommand As String, ByVal sArguments As String) As Boolean
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Function
+
+
+Public Function performVideoEdit(ByVal viInputs() As InputInfo)
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Function
+
+
+Public Function runConcatCommand(
+        ByVal workFiles() As String, ByVal outFile As String) As Boolean
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Function
+
+
+Public Function runSegmentCommand(
+        ByVal viSrcInfo As InputInfo, ByVal workOutFile As String) As Boolean
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Function
+
+
+End Module
