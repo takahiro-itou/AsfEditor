9e97e538dc548b13e66b1bc22f6d6053fb194426
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-01 15:56:53 +0900

    メインビューを実装

--- AsfEditor/MainView.vb
+++ AsfEditor/MainView.vb
@@ -1,5 +1,72 @@
 ﻿Public Class MainView
 
+Private m_nInputCount As Integer
+Private m_viInputList() As InputInfo
+
+
+Private Function addFileToList(ByVal fileName As Strign) As Boolean
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+    ReDim Preserve m_viInputList(m_nInputCount)
+    m_viInputList(m_nInputCount) = New InputInfo()
+    With m_viInputList(m_nInputCount)
+        .FileName = fileName
+        .sStartTime = "00:00:00.000"
+        .sEndTime = "00:00:00.000"
+        .sTimeDuration = ""
+        .bConcat = True
+    End With
+    m_nInputCount = m_nInputCount + 1
+
+    updateGridView()
+    addFileToList = True
+End Function
+
+
+Private Sub openInputFile()
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+    With dlgOpen
+        .DefaultExt = ".wmv"
+        .Filter = "asf file(*.wmv;*.asf)|*.wmv;*.asf|All files(*.*)|*.*"
+        .FilterIndex = 1
+
+        If .ShowDialog() = DialogResult.OK Then
+            addFileToList(.FileName)
+        End If
+    End With
+
+End Sub
+
+
+Private Function removeFileFromList() As Boolean
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Function
+
+
+Private Sub showSaveFileDialog(ByRef targetTextBox As TextBox)
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Sub
+
+
+Private Sub updateGridView()
+''--------------------------------------------------------------------
+''
+''--------------------------------------------------------------------
+
+End Sub
+
+
 Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles _
             btnEdit.Click, mnuEditTime.Click
 ''--------------------------------------------------------------------
