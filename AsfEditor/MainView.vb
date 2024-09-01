056026112c96ebcbd835389b8565f89a00960c37
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-01 15:02:21 +0900

    編集ビューを表示するためのコードを仮実装

--- AsfEditor/MainView.vb
+++ AsfEditor/MainView.vb
@@ -1,5 +1,28 @@
 ﻿Public Class MainView
 
+Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles _
+            btnEdit.Click, mnuEditTime.Click
+''--------------------------------------------------------------------
+''    「編集」ボタンのクリックイベントハンドラ。
+''    メニュー「ファイル」－「終了」
+''--------------------------------------------------------------------
+Dim selIndex As Integer
+
+    For Each r As DataGridViewRow In dgvInputs.SelectedRows
+        selIndex = r.Index - 1
+    Next r
+
+    Using frmEdit As New EditTimeForm()
+        With frmEdit
+            .ShowDialog(Me)
+
+            .Dispose()
+        End With
+    End Using
+
+End Sub
+
+
 Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
             mnuFileExit.Click
 ''--------------------------------------------------------------------
