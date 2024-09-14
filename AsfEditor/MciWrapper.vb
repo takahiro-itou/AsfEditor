07b9cd4482d8e6a5aab659037a6df829a623fb6f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-14 18:00:11 +0900

    API の宣言

--- AsfEditor/MciWrapper.vb
+++ AsfEditor/MciWrapper.vb
@@ -1,3 +1,21 @@
 Public Class MciWrapper
 
+<System.Runtime.InteropServices.DllImport("winmm.dll",
+    CharSet:=System.Runtime.InteropServices.CharSet.Auto)>
+Private Shared Function mciSendString(
+        ByVal lpszCommand As String,
+        ByVal lpszReturnString As System.Text.StringBuilder,
+        ByVal cchReturn As Integer,
+        ByVal hwndCallback As IntPtr) As Integer
+End Function
+
+<System.Runtime.InteropServices.DllImport("winmm.dll",
+    CharSet:=System.Runtime.InteropServices.CharSet.Auto)>
+Private Shared Function mciGetErrorString(
+        ByVal fdwError As Integer,
+        ByVal lpszErrorText As System.Text.StringBuilder,
+        ByVal cchErrorText As Integer) As Integer
+End Function
+
+
 End Class
