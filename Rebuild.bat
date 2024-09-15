ad187a03f215d8f310bea67f99a2b62c16e7eb05
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-15 10:56:14 +0900

    バッチファイルを更新
    (1) ターゲットプラットフォームを x86(Win32) に変更

--- Rebuild.bat
+++ Rebuild.bat
@@ -1,2 +1,3 @@
 
-msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64
+REM msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64
+msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x86
