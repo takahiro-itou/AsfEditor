736a4594de137d2e9ed5ddb472c99ec17e6d6ec2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-09-16 11:35:48 +0900

    ビルド用のバッチファイルを更新
    (1) デバッグとリリースを両方ともビルド

--- Rebuild.bat
+++ Rebuild.bat
@@ -1,3 +1,4 @@
 
 REM msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x64
+msbuild -restore -t:Rebuild -p:Configuration="Debug"   -p:Platform=x86
 msbuild -restore -t:Rebuild -p:Configuration="Release" -p:Platform=x86
